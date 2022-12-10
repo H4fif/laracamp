7d8a2c5ca22e4fce0e4d9935270464e2c8127291
Hafif <hafifimammuddyn@gmail.com>
2022-12-10 23:00:36 +0700

    ♻️ replace url redirected to dashboard

--- resources/views/emails/checkout/afterCheckout.blade.php
+++ resources/views/emails/checkout/afterCheckout.blade.php
@@ -5,8 +5,8 @@ Hi {{ $checkout->User->name }}
 <br />
 Your registration on <b>{{ $checkout->Camp->title }}</b> has been received, please proceed to payment by clicking the button below.
 
-@component('mail::button', ['url' => route('user.checkout.invoice', $checkout->id)])
-Get Invoice
+@component('mail::button', ['url' => route('user.dashboard')])
+My Dashboard
 @endcomponent
 
 Thanks,<br>
