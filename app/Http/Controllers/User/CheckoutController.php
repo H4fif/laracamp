8ebb06cef118b39bfc3cffa284df307ffa0e0ffd
Hafif <hafifimammuddyn@gmail.com>
2022-12-10 23:01:38 +0700

    🔥 remove unused codes

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -112,9 +112,4 @@ class CheckoutController extends Controller
     {
         return view('checkout.success');
     }
-
-    public function invoice(Checkout $checkout)
-    {
-        return $checkout;
-    }
 }
