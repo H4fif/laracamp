ca6baf0ee8e4ed02ae6d28e3cdf5eb86dc94d68e
Hafif <hafifimammuddyn@gmail.com>
2022-12-10 21:55:07 +0700

    ♻️ Added send mail after successful checkout

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -8,6 +8,8 @@ use App\Models\Checkout;
 use Illuminate\Http\Request;
 use App\Http\Requests\User\Checkout\Store;
 use Auth;
+use Mail;
+use App\Mail\Checkout\AfterCheckout;
 
 class CheckoutController extends Controller
 {
@@ -55,6 +57,8 @@ class CheckoutController extends Controller
         $user->save();
 
         $checkout = Checkout::create($data);
+
+        Mail::to(Auth::user()->email)->send(new AfterCheckout($checkout));
 
         return redirect()->route('checkout.success');
     }
@@ -107,5 +111,10 @@ class CheckoutController extends Controller
     public function success()
     {
         return view('checkout.success');
+    }
+
+    public function invoice(Checkout $checkout)
+    {
+        return $checkout;
     }
 }
