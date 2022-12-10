d61236cfd711e694f694854381c30e519375fcdb
Hafif <hafifimammuddyn@gmail.com>
2022-12-11 00:11:37 +0700

    ♻️ slicing dashboard admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,61 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="dashboard my-5">
-        <div class="container">
-            <div class="row text-left">
-                <div class=" col-lg-12 col-12 header-wrap mt-4">
-                    <p class="story">
-                        DASHBOARD
-                    </p>
-                    <h2 class="primary-header ">
-                        My Bootcamps
-                    </h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="card">
+                    <div class="card-header">
+                        My Camps
+                    </div>
+
+                    <div class="card-body overflow-scroll">
+                        @include('components.alert')
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>User</th>
+                                    <th>Camp</th>
+                                    <th>Price</th>
+                                    <th>Register Data</th>
+                                    <th>Paid Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                @forelse ($checkouts as $checkout)
+                                    <tr>
+                                        <td>{{ $checkout->user->name }}</td>
+                                        <td>{{ $checkout->camp->title }}</td>
+                                        <td>{{ $checkout->camp->price }}</td>
+                                        <td>{{ $checkout->created_at->format('M d Y') }}</td>
+
+                                        <td>
+                                            @if ($checkout->is_paid)
+                                                <span class="badge bg-success">Paid</span>
+                                            @else
+                                                <span class="badge bg-warning">Waiting</span>
+                                            @endif
+                                        </td>
+
+                                        <td>
+                                            @if (!$checkout->is_paid)
+                                                <form action="{{ route('admin.checkout.confirm', $checkout->id) }}" method="post">
+                                                    @csrf
+                                                    <button class="btn btn-primary btn-sm p-2">Confirm Payment</button>
+                                                </form>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="3">No camps registered</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-            <div class="row my-5">
-                @include('components.alert')
-
-                <table class="table">
-                    <tbody>
-                        @forelse ($checkouts as $checkout)
-                            <tr class="align-middle">
-                                <td width="18%">
-                                    <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="">
-                                </td>
-                                <td>
-                                    <p class="mb-2">
-                                        <strong>{{ $checkout->camp->title }}</strong>
-                                    </p>
-                                    <p>
-                                        {{ $checkout->created_at->format('F d, Y') }}
-                                    </p>
-                                </td>
-                                <td>
-                                    <strong>${{ number_format($checkout->camp->price, 0) }}k </strong>
-                                </td>
-                                <td>
-                                    @if ($checkout->is_paid)
-                                        <strong><span class="text-green">Payment Success</span></strong>
-                                    @else
-                                        <strong><span class="text-warning">Waiting for Payment</span></strong>
-                                    @endif
-                                </td>
-                                <td>
-                                    <a href="https://wa.me/08xxx?text=Hai, Saya ingin bertanya tentang kelas {{ $checkout->camp->title }}"
-                                        class="btn btn-primary">
-                                        Get Invoice
-                                    </a>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr colspan="5">
-                                <td>No data</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
-            </div>
         </div>
-    </section>
+    </div>
 @endsection
