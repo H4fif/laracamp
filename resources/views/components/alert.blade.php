8ee65be3402976ca4ee24f0c49e49ae56e08c5dc
Hafif <hafifimammuddyn@gmail.com>
2022-12-11 00:11:16 +0700

    ✨Added alert for success event

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -4,3 +4,10 @@
         <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
+
+@if ($message = Session::get('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>{{ $message }}</strong>
+        <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
